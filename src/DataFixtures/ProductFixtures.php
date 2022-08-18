13c818470c1a0816affc65b9841184e697e13ff0
muratcakmakis <murat.cakmak@ideasoft.com.tr>
2022-08-18 11:32:24 +0300

    update setCategory in ProductFixtures.php

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -5,44 +5,39 @@ namespace App\DataFixtures;
 use App\Entity\Category;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
 
 class ProductFixtures extends Fixture
 {
+
     public function load(ObjectManager $manager): void
     {
         $data = [
             [
-                "id" => 100,
                 "name" => "Black&Decker A7062 40 Parça Cırcırlı Tornavida Seti",
                 "category_id" => 1,
                 "price" => 120.75,
                 "stock" => 10
             ],
             [
-                'id' => 101,
                 'name' => 'Reko Mini Tamir Hassas Tornavida Seti 32\'li',
                 'category_id' => 1,
                 'price' => 49.50,
                 'stock' => 10,
             ],
             [
-                'id' => 102,
                 'name' => 'Viko Karre Anahtar - Beyaz',
                 'category_id' => 2,
                 'price' => 11.28,
                 'stock' => 10,
             ],
             [
-                'id' => 103,
                 'name' => 'Legrand Salbei Anahtar, Alüminyum',
                 'category_id' => 2,
                 'price' => 22.80,
                 'stock' => 10,
             ],
             [
-                'id' => 104,
                 'name' => 'Schneider Asfora Beyaz Komütatör',
                 'category_id' => 2,
                 'price' => 12.95,
@@ -51,12 +46,12 @@ class ProductFixtures extends Fixture
         ];
 
         $categories = $manager->getRepository(Category::class)->findAll();
+        $categoryCount = count($categories);
 
-        foreach ($data as $item){
-
+        foreach ($data as $item) {
             $product = new Product();
             $product->setName($item['name']);
-            $product->setCategory($categories[$item['category_id']]);
+            $product->setCategory($categories[rand(0, $categoryCount - 1)]);
             $product->setPrice($item['price']);
             $product->setStock($item['stock']);
             $manager->persist($product);
