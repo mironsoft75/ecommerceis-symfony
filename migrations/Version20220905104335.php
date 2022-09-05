b4dedf940fa8c830f6d694d1253051289393628a
muratcakmakis <murat.cakmak@ideasoft.com.tr>
2022-09-05 13:55:08 +0300

    updated migration

--- migrations/Version20220905104335.php
+++ migrations/Version20220905104335.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220902083920 extends AbstractMigration
+final class Version20220905104335 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -28,10 +28,12 @@ final class Version20220902083920 extends AbstractMigration
         $this->addSql('CREATE TABLE categories (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE customers (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, since DATE NOT NULL, revenue NUMERIC(13, 2) DEFAULT \'0\' NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE discounts (id SERIAL NOT NULL, type INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(2500) DEFAULT NULL, status BOOLEAN NOT NULL, json_data TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE order_discount_history (id SERIAL NOT NULL, order_id INT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(2500) DEFAULT NULL, json_data TEXT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_F3A409468D9F6D38 ON order_discount_history (order_id)');
         $this->addSql('CREATE TABLE order_product (id SERIAL NOT NULL, order_id INT NOT NULL, product_id INT NOT NULL, quantity INT NOT NULL, unit_price NUMERIC(13, 2) DEFAULT \'0\' NOT NULL, total NUMERIC(13, 2) DEFAULT \'0\' NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_2530ADE68D9F6D38 ON order_product (order_id)');
         $this->addSql('CREATE INDEX IDX_2530ADE64584665A ON order_product (product_id)');
-        $this->addSql('CREATE TABLE orders (id SERIAL NOT NULL, customer_id INT NOT NULL, total NUMERIC(13, 2) DEFAULT \'0\' NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE orders (id SERIAL NOT NULL, customer_id INT NOT NULL, sub_total NUMERIC(13, 2) DEFAULT \'0\' NOT NULL, total NUMERIC(13, 2) DEFAULT \'0\' NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_E52FFDEE9395C3F3 ON orders (customer_id)');
         $this->addSql('CREATE TABLE products (id SERIAL NOT NULL, category_id INT NOT NULL, name VARCHAR(255) NOT NULL, price NUMERIC(13, 2) DEFAULT \'0\' NOT NULL, stock INT DEFAULT 0 NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_B3BA5A5A12469DE2 ON products (category_id)');
@@ -50,6 +52,7 @@ final class Version20220902083920 extends AbstractMigration
         $this->addSql('ALTER TABLE cart_product ADD CONSTRAINT FK_2890CCAA1AD5CDBF FOREIGN KEY (cart_id) REFERENCES carts (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE cart_product ADD CONSTRAINT FK_2890CCAA4584665A FOREIGN KEY (product_id) REFERENCES products (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE carts ADD CONSTRAINT FK_4E004AAC9395C3F3 FOREIGN KEY (customer_id) REFERENCES customers (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE order_discount_history ADD CONSTRAINT FK_F3A409468D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE order_product ADD CONSTRAINT FK_2530ADE68D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE order_product ADD CONSTRAINT FK_2530ADE64584665A FOREIGN KEY (product_id) REFERENCES products (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE orders ADD CONSTRAINT FK_E52FFDEE9395C3F3 FOREIGN KEY (customer_id) REFERENCES customers (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -59,9 +62,11 @@ final class Version20220902083920 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE cart_product DROP CONSTRAINT FK_2890CCAA1AD5CDBF');
         $this->addSql('ALTER TABLE cart_product DROP CONSTRAINT FK_2890CCAA4584665A');
         $this->addSql('ALTER TABLE carts DROP CONSTRAINT FK_4E004AAC9395C3F3');
+        $this->addSql('ALTER TABLE order_discount_history DROP CONSTRAINT FK_F3A409468D9F6D38');
         $this->addSql('ALTER TABLE order_product DROP CONSTRAINT FK_2530ADE68D9F6D38');
         $this->addSql('ALTER TABLE order_product DROP CONSTRAINT FK_2530ADE64584665A');
         $this->addSql('ALTER TABLE orders DROP CONSTRAINT FK_E52FFDEE9395C3F3');
@@ -71,6 +76,7 @@ final class Version20220902083920 extends AbstractMigration
         $this->addSql('DROP TABLE categories');
         $this->addSql('DROP TABLE customers');
         $this->addSql('DROP TABLE discounts');
+        $this->addSql('DROP TABLE order_discount_history');
         $this->addSql('DROP TABLE order_product');
         $this->addSql('DROP TABLE orders');
         $this->addSql('DROP TABLE products');
